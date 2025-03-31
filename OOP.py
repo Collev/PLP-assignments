a0d38879058765f18d33a0b4b39eea6b9998cac6
Collev <calkaey353@gmail.com>
2025-04-01 01:59:14 +0300

    OOP assignment commit

---
+++ OOP.py
@@ -0,0 +1,28 @@
+
+class Book:
+    def __init__(self, title, author, pages):
+        self._title = title  # Encapsulated attribute
+        self._author = author
+        self._pages = pages
+
+    def read(self):
+        print(f"You start reading '{self._title}' by {self._author}.")
+
+    def book_info(self):
+        return f"'{self._title}' by {self._author} - {self._pages} pages"
+
+
+
+class EBook(Book):
+    def __init__(self, title, author, pages, file_size):
+        super().__init__(title, author, pages)
+        self._file_size = file_size
+
+    def download(self):
+        print(f"Downloading '{self._title}' - {self._file_size} MB.")
+
+    def book_info(self):
+        base_info = super().book_info()
+        return f"{base_info}, File Size: {self._file_size} MB"
+
+
